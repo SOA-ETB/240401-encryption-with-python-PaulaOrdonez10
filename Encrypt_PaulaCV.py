fb5ddd42105bb5b6284fd4a745f83f7ea59009ba
PaulaOrdonez10 <paulaordonez@schoolofautomation.tech>
2024-04-09 13:50:26 +0100

    change with exception done

--- Encrypt_PaulaCV.py
+++ Encrypt_PaulaCV.py
@@ -1,26 +1,28 @@
 import aspose.words as aw
 import os
 
-# Input file path
-input_file_path = "C:\\Users\\Administrator\\OneDrive - School of Automation\\Blue Prism Advance SOA\\PythonEncryptionProject\\Phyton_Encryption_Files\\Paula_cv.docx"
+try:
+    # Input file path
+    input_file_path = "C:\\Users\\Administrator\\OneDrive - School of Automation\\Blue Prism Advance SOA\\PythonEncryptionProject\\Phyton_Encryption_Files\\Paula_cv.docx"
 
-# Output file path with password
-output_file_path = "C:\\Users\\Administrator\\OneDrive - School of Automation\\Blue Prism Advance SOA\\Phyton_task\\Phyton_Encryption_Files\\document-password-protected.docx"
+    # Output file path with password
+    output_file_path = "C:\\Users\\Administrator\\OneDrive - School of Automation\\Blue Prism Advance SOA\\Phyton_task\\Phyton_Encryption_Files\\document-password-protected.docx"
 
-# Load the document
-doc = aw.Document(input_file_path)
+    # Load the document
+    doc = aw.Document(input_file_path)
 
-# Create document options
-options = aw.saving.OoxmlSaveOptions(aw.SaveFormat.DOCX)
+    # Create document options
+    options = aw.saving.OoxmlSaveOptions(aw.SaveFormat.DOCX)
 
-# Set password
-options.password = "Paula10"
+    # Set password
+    options.password = "Paula10"
 
-# Save the updated document
-doc.save(output_file_path, options)
+    # Save the updated document
+    doc.save(output_file_path, options)
 
-print(f"Document with password saved in: {output_file_path}")
+    print(f"Document with password saved in: {output_file_path}")
 
-
+except Exception as e:
+    print(f'An error occurred: {e}')
 
 
